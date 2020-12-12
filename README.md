227f3055f53b4e8cce156f4bfbd04fb5562b52a8
Alan F <alanworking1@gmail.com>
2020-12-12 19:36:07 -0300

    sending project

---
+++ README.md
@@ -0,0 +1,11 @@
+writing...
+
+````
+git clone https://github.com/alandev2/keyboard-macro-text.git
+```
+````
+npm install
+```
+````
+npm start
+```
---
+++ src/index.js
@@ -0,0 +1,41 @@
+const ioHook = require('iohook');
+var ks = require('node-key-sender');
+
+const keyboards = require('../keyboards.json');
+const { print_key_when_press } = require('../config.json');
+
+function findKeyMacroWithKeyPress({ shiftKey, altKey, ctrlKey, rawcode }){
+  return keyboards.find(
+  (config) => 
+    ((config.shiftKey === shiftKey) && 
+    (config.altKey === altKey) && 
+    (config.ctrlKey === ctrlKey) && 
+    (config.rawcode === rawcode))
+  );
+}
+
+
+ks.setKeyboardLayout('slash');
+
+function teste(){
+ ks.sendKey("@92");
+}
+
+ioHook.on('keypress', async (keyPress) => {
+  const findedKey = findKeyMacroWithKeyPress(keyPress);
+
+  if(findedKey){
+    ks.sendKey(findedKey.sendCode);
+    keyPress.finded_macro = findedKey ;
+  }
+
+  print_key_when_press ? console.log({ keyPress }) : null;
+})
+
+ioHook.start(); // starting
+
+// Teste your typing here 😁
+//
+//
+//
+//
